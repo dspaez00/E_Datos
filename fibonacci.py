9201528fa32abe8f012de36f8fc40236e37c3c09
dspaez00 <60805442+dspaez00@users.noreply.github.com>
2020-02-08 00:31:32 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,18 @@
 
 def fibo(n):
 	# Escriba aquí el código para calcular el n-esimo valor de la serie de fibonacci de forma no recursiva (use ciclos).
+	cont=o
+	print("Que posición desea conocer")
+	pos=input()
+	while cont =< pos:
+		sum = sum + cont
+		cont = cont + 1
+	print('El número de la posición {} es {}'.format(pos,sum))
 	pass
 
 def fibo_rec(n):
 	# Escriba aquí el código para calcular el n-esimo valor de la serie de fibonacci de forma recursiva (NO use ciclos).
+	
 	pass
 	
 
