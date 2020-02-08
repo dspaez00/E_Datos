81f0e7ef88214fa3bbea8625d79aa7348de9a6b6
dspaez00 <60805442+dspaez00@users.noreply.github.com>
2020-02-07 23:46:07 -0500

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+
+def fibo(n):
+	# Escriba aquí el código para calcular el n-esimo valor de la serie de fibonacci de forma no recursiva (use ciclos).
+	pass
+
+def fibo_rec(n):
+	# Escriba aquí el código para calcular el n-esimo valor de la serie de fibonacci de forma recursiva (NO use ciclos).
+	pass
+	
+
+print(fibo(10))
+print(fibo_rec(10))
+	
